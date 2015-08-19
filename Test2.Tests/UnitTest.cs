58e87d89631812cce1b01609f90eb02ac1fb5f55
Антон Александров <BobsAnton@gmail.com>
2015-08-19 15:53:40 +0300

    Добавляет модульные тесты

--- Test2.Tests/UnitTest.cs
+++ Test2.Tests/UnitTest.cs
@@ -16,5 +16,21 @@ namespace Test2.Tests
             ViewResult result = controller.Compute("(2+3)*2+4/2");
             Assert.AreEqual(12, result.Model); 
         }
+
+        [TestMethod]
+        public void TestSpaces()
+        {
+            HomeController controller = new HomeController();
+            ViewResult result = controller.Compute("2 + 3");
+            Assert.AreEqual(5, result.Model); 
+        }
+
+        [TestMethod]
+        public void TestUnaryMinus()
+        {
+            HomeController controller = new HomeController();
+            ViewResult result = controller.Compute("-2+300");
+            Assert.AreEqual(298, result.Model); 
+        }
     }
 }
